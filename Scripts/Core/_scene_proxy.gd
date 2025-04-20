6a7a1707f549aee98a77a322f2fceff261cd5e42
jponzo <jeremy.ponzo@gmail.com>
2025-04-20 19:27:01 +0200

    add lights to scene proxy

--- Scripts/Core/_scene_proxy.gd
+++ Scripts/Core/_scene_proxy.gd
@@ -3,7 +3,24 @@ class_name _TL_SceneProxy
 class MeshProxy:
 	var model_matrix : Projection
 	var index_data : PackedInt32Array
-	var vertex_data : PackedVector3Array
+	var position_data : PackedVector3Array
+	var normal_data : PackedVector3Array
+	var uv_data : PackedVector2Array
+
+class _LightProxy:
+	var color : Color
+	var intensity : float
+
+class OmniLightProxy extends _LightProxy:
+	var location : Vector3
+
+class DirectionalLightProxy extends  _LightProxy:	
+	var direction : Vector3
+
+class SpotLightProxy extends  _LightProxy:	
+	var location : Vector3
+	var direction : Vector3
+	var angle : float
 
 class CameraProxy:
 	var view_matrix : Projection
@@ -16,6 +33,15 @@ func get_current_camera() -> CameraProxy:
 	return null
 	
 func get_meshes() -> Array[MeshProxy]:
+	return []
+	
+func get_omni_lights() -> Array[OmniLightProxy]:
+	return []
+
+func get_spot_lights() -> Array[SpotLightProxy]:
+	return []
+	
+func get_directional_lights() -> Array[DirectionalLightProxy]:
 	return []
 	
 func _cleanup() -> void:
