f3ecd7be02f2c866ba5b7622819c3a5e8e97d9fa
jponzo <jeremy.ponzo@gmail.com>
2025-06-13 23:05:42 +0200

    node enter/exit tree notifications

--- Scripts/Core/_scene_proxy.gd
+++ Scripts/Core/_scene_proxy.gd
@@ -1,5 +1,53 @@
 class_name _TL_SceneProxy
 
+var _scene_root : Node
+
+func _setup(scene_root : Node) -> void:
+	_scene_root = scene_root
+
+	var all_nodes : Array = _find_all_in_tree(_scene_root, func(node : Node): return true)
+	for node in all_nodes:
+		on_node_enter_tree(node)
+
+	_scene_root.get_tree().node_added.connect(on_node_enter_tree)
+	_scene_root.get_tree().node_removed.connect(on_node_exit_tree)
+
+func on_node_enter_tree(node: Node) -> void:
+	pass
+
+func _cleanup() -> void:
+	_scene_root.get_tree().node_added.disconnect(on_node_enter_tree)
+	_scene_root.get_tree().node_removed.disconnect(on_node_exit_tree)
+
+	var all_nodes : Array = _find_all_in_tree(_scene_root, func(node : Node): return true)
+	for node in all_nodes:
+		on_node_exit_tree(node)
+
+func on_node_exit_tree(node : Node) -> void:
+	pass
+
+func _find_first_in_tree(root : Node, selector : Callable) -> Variant:
+	if selector.call(root):
+		return root
+		
+	for child in root.get_children():
+		var selected =  _find_first_in_tree(child, selector)
+		if selected != null:
+			return selected
+	
+	return null
+	
+func _find_all_in_tree(root : Node, selector : Callable) -> Array[Variant]:
+	var all_selected : Array[Variant]
+	
+	if selector.call(root):
+		all_selected.append(root)
+		
+	for child in root.get_children():
+		all_selected.append_array(_find_all_in_tree(child, selector))
+	
+	return all_selected
+
 class ProxyDataProcessor:
 	var proxy_data : ProxyData
 
@@ -104,9 +152,6 @@ class CameraProxy extends ProxyObject:
 	var view_matrix : Projection
 	var projection_matrix : Projection
 
-func _setup(scene_root : Node) -> void:
-	pass
-
 func _update() -> void:
 	pass
 
@@ -124,6 +169,3 @@ func get_spot_lights() -> Array[SpotLightProxy]:
 	
 func get_directional_lights() -> Array[DirectionalLightProxy]:
 	return []
-	
-func _cleanup() -> void:
-	pass
