efa00b507504c8e3d9ef82c1de392963654d49d8
jponzo <jeremy.ponzo@gmail.com>
2025-06-15 20:46:40 +0200

    remove model from renderer def

--- Scripts/Definitions/renderer_def.gd
+++ Scripts/Definitions/renderer_def.gd
@@ -1,7 +1,6 @@
 extends Resource
 class_name TL_RendererDef
 
-@export var model_scrit : GDScript
 @export var renderer_script : GDScript
 @export var scene_proxy_script : GDScript
 @export var renderer_pass_defs : Array[TL_RenderPassDef]
