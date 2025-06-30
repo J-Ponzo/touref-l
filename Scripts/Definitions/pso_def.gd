d2c624452d353f6908c072097f97607d163741b6
JPonzo <jeremy.ponzo@gmail.com>
2025-06-30 13:54:05 +0200

    WIP Refact defs (add material feature flags to pso_def)

--- Scripts/Definitions/pso_def.gd
+++ Scripts/Definitions/pso_def.gd
@@ -4,3 +4,4 @@ class_name TL_PSODef
 @export var vertex_shader : TL_GLSLShader
 @export var fragment_shader : TL_GLSLShader
 @export var vertex_format_def : TL_VertexFormatDef
+@export var material_features_def : TL_MaterialFeatureFlags_Def
