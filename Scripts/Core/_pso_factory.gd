edd8ddd352a12a0e25ea68f6763999284cae581f
jponzo <jeremy.ponzo@gmail.com>
2025-08-06 21:07:45 +0200

    WIP decoupling material/pso (extract generated logics)

--- Scripts/Core/_pso_factory.gd
+++ Scripts/Core/_pso_factory.gd
@@ -1,13 +1,23 @@
 class_name _TL_PSOFactory
 
+var generated_pso_def : TL_GeneratedPSODef
+var framebuffer_format : int
+var nb_color_attachments : int
+
 var mask_lookup : Dictionary[Object, int]
 var pso_lookup : Dictionary[int, _TL_PSO]
 
-func get_or_create_pso_from_object(object : Object, generated_pso_def : TL_GeneratedPSODef) -> _TL_PSO:
-    if !mask_lookup.has(object):
-        _create_pso_from_object(object, generated_pso_def)
-    var mask = mask_lookup[object]
-    return pso_lookup[mask]
+func get_or_create_pso_from_object(object : Object) -> _TL_PSO:
+	object.get_instance_id()
+	if !mask_lookup.has(object):
+		_create_pso_from_object(object)
+	var mask = mask_lookup[object]
+	return pso_lookup[mask]
 
-func _create_pso_from_object(object : Object, generated_pso_def : TL_GeneratedPSODef) -> void:
-    pass
+func _create_pso_from_object(object : Object) -> void:
+	pass
+
+func _cleanup() -> void:
+	for pso_inst in pso_lookup.values():
+		_TL_Renderer_Factory.rd.free_rid(pso_inst.pipeline)
+		_TL_Renderer_Factory.rd.free_rid(pso_inst.shader_program)
--- Scripts/Core/_render_pass.gd
+++ Scripts/Core/_render_pass.gd
@@ -1,10 +1,12 @@
 class_name _TL_RenderPass
 
+const ERR_PSOFACTORY_WRONG_PARENT = "TourefL : Cannot instantiate the pso factory from class %s. It must inherit from _TL_PSOFactory."
+
 var render_pass_def : TL_RenderPassDef
 
-var pso_name_to_mask : Dictionary[StringName, int]
-var pso_instances : Dictionary[int, _TL_PSO]
-# var attachments : Dictionary[StringName, RID]
+# var pso_name_to_mask : Dictionary[StringName, int]
+# var pso_instances : Dictionary[int, _TL_PSO]
+var pso_factories : Dictionary[StringName, _TL_PSOFactory]
 
 var explicits_pso : Dictionary[StringName, _TL_PSO]
 
@@ -20,19 +22,34 @@ func _setup() -> void:
 	var attachments : Array[RID] = renderer.get_attachments(render_pass_def.fb_format_def.get_all_attachment_keys())
 	framebuffer = renderer.rd.framebuffer_create(attachments, framebuffer_format)
 	explicits_pso = create_explicits_pso_from_defs(render_pass_def.pso_defs)
+	pso_factories = create_pso_factories_from_defs(render_pass_def.generated_pso_defs, framebuffer_format, get_nb_color_attachments(render_pass_def.fb_format_def))
 	# pso_name_to_mask = create_pso_name_to_mask(pso_defs)
 	# pso_instances = create_piplines_from_defs(pso_defs, pso_name_to_mask)
 
-func get_or_create_pso_instance(mat_feature_flags_mask : int) -> _TL_PSO:
-	if !pso_instances.has(mat_feature_flags_mask):
-		var pso_def = _TL_Renderer_Factory.get_pso_def_from_mask_and_shaders(mat_feature_flags_mask, render_pass_def.uber_vertex_shader, render_pass_def.uber_fragment_shader)
-		pso_instances[mat_feature_flags_mask] = _TL_Renderer_Factory.create_pso(pso_def, framebuffer_format, get_nb_color_attachments(render_pass_def.fb_format_def))
-	return pso_instances[mat_feature_flags_mask]
+# func get_or_create_pso_instance(mat_feature_flags_mask : int) -> _TL_PSO:
+# 	if !pso_instances.has(mat_feature_flags_mask):
+# 		var pso_def = _TL_Renderer_Factory.get_pso_def_from_mask_and_shaders(mat_feature_flags_mask, render_pass_def.uber_vertex_shader, render_pass_def.uber_fragment_shader)
+# 		pso_instances[mat_feature_flags_mask] = _TL_Renderer_Factory.create_pso(pso_def, framebuffer_format, get_nb_color_attachments(render_pass_def.fb_format_def))
+# 	return pso_instances[mat_feature_flags_mask]
 
-func create_explicits_pso_from_defs(pso_defs : Dictionary[StringName, TL_ExpicitPSODef]) -> Dictionary[StringName, _TL_PSO]:
+func create_pso_factories_from_defs(generated_pso_defs : Dictionary[StringName, TL_GeneratedPSODef], framebuffer_format : int, nb_color_attachment : int) -> Dictionary[StringName, _TL_PSOFactory]:
+	var factories : Dictionary[StringName, _TL_PSOFactory]
+	for key in generated_pso_defs.keys():
+		var generated_pso_def : TL_GeneratedPSODef = generated_pso_defs[key]
+		var pso_factory_inst = generated_pso_def.factory_script.new()
+		if pso_factory_inst is _TL_PSOFactory:
+			pso_factory_inst.generated_pso_def = generated_pso_def
+			pso_factory_inst.framebuffer_format = framebuffer_format
+			pso_factory_inst.nb_color_attachments = nb_color_attachment
+			factories[key] = pso_factory_inst
+		else :
+			push_error(ERR_PSOFACTORY_WRONG_PARENT % generated_pso_def.factory_script)
+	return factories
+
+func create_explicits_pso_from_defs(explicit_pso_defs : Dictionary[StringName, TL_ExpicitPSODef]) -> Dictionary[StringName, _TL_PSO]:
 	var pso_instances : Dictionary[StringName, _TL_PSO] 
-	for key in pso_defs.keys():
-		pso_instances[key] = _TL_Renderer_Factory.create_pso(pso_defs[key], framebuffer_format, get_nb_color_attachments(render_pass_def.fb_format_def))
+	for key in explicit_pso_defs.keys():
+		pso_instances[key] = _TL_Renderer_Factory.create_pso(explicit_pso_defs[key], framebuffer_format, get_nb_color_attachments(render_pass_def.fb_format_def))
 	return pso_instances
 
 func create_pso_name_to_mask(pso_defs : Dictionary[StringName, TL_ExpicitPSODef]) -> Dictionary[StringName, int]:
@@ -76,15 +93,12 @@ func get_nb_color_attachments(fb_format_def : TL_FramebufferFormat_Def) -> int :
 	
 func _cleanup() -> void:
 	renderer.rd.free_rid(framebuffer)
-	for pso_inst in pso_instances.values():
-		renderer.rd.free_rid(pso_inst.pipeline)
-		renderer.rd.free_rid(pso_inst.shader_program)
-	pso_name_to_mask.clear()
-	pso_instances.clear()
-
-	# for attachment in attachments.values():
-	# 	renderer.rd.free_rid(attachment)
-	# attachments.clear()
+	for pso_factorie in pso_factories.values():
+		pso_factorie._cleanup()
+	explicits_pso.clear()
+	pso_factories.clear()
+	# pso_name_to_mask.clear()
+	# pso_instances.clear()
 	
 
 func _render() -> void:
