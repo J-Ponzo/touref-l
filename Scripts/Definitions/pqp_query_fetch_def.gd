26ef452e1d6edf20d0704ad0f2d38ebd2bfaf025
jponzo <jeremy.ponzo@gmail.com>
2025-08-11 20:42:33 +0200

    WIP proxy_queues_manager (fix query fetch processor)

--- Scripts/Definitions/pqp_query_fetch_def.gd
+++ Scripts/Definitions/pqp_query_fetch_def.gd
@@ -1,7 +1,6 @@
 extends TL_ProxyQueueProcessorDef
 class_name TL_PQPQueryFetchDef
 
-@export var type : StringName
 @export var query_def : TL_FeatureFlagQueryDef
 
 var _query : _TL_FeatureFlagManager.FeatureFlagQuery
@@ -10,6 +9,6 @@ func _setup(proxy_model : _TL_ProxyModel) -> void:
 	_query = proxy_model.renderer.feature_flag_manager.build_query_from_def(query_def)
 
 func _process(proxy_model : _TL_ProxyModel, data : Array[_TL_ProxyData]) -> Array[_TL_ProxyData]:
-	var processed_array = []
+	var processed_array : Array[_TL_ProxyData] = []
 	processed_array.append_array(proxy_model.renderer.scene_proxy.get_data_from_query(_query))
 	return processed_array
