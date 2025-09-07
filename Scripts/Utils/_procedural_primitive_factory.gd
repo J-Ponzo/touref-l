39977050ef88dbd9c4bc639b2e772c46cbfd9734
JPonzo <jeremy.ponzo@gmail.com>
2025-09-07 17:35:52 +0200

    reorient unit cone

--- Scripts/Utils/_procedural_primitive_factory.gd
+++ Scripts/Utils/_procedural_primitive_factory.gd
@@ -21,6 +21,102 @@ static func create_screen_quad(setup_uv : bool = false) -> TL_Procedural_Primiti
 
 	return primitive
 
+static func create_inverted_sphere(stacks : int = 32, slices : int = 32) -> TL_Procedural_Primitive:
+	var primitive : TL_Procedural_Primitive = TL_Procedural_Primitive.new()
+
+	var positions : PackedVector3Array = []
+	var indices : PackedInt32Array = []
+
+	for i in range(stacks + 1):
+		var v = float(i) / stacks
+		var theta = v * PI
+		var sin_theta = sin(theta)
+		var cos_theta = cos(theta)
+
+		for j in range(slices + 1):
+			var u = float(j) / slices
+			var phi = u * TAU
+
+			var x = sin_theta * cos(phi)
+			var y = cos_theta
+			var z = sin_theta * sin(phi)
+
+			positions.append(Vector3(x, y, z))
+
+	for i in range(stacks):
+		for j in range(slices):
+			var first = i * (slices + 1) + j
+			var second = first + slices + 1
+
+			indices.append(first)
+			indices.append(second + 1)
+			indices.append(second)
+
+			indices.append(first)
+			indices.append(first + 1)
+			indices.append(second + 1)
+
+	var byte_array : PackedByteArray = positions.to_byte_array()
+	primitive.position_buffer = _TL_Renderer_Factory.rd.vertex_buffer_create(byte_array.size(), byte_array)
+
+	byte_array = indices.to_byte_array()
+	primitive.index_buffer = _TL_Renderer_Factory.rd.index_buffer_create(indices.size(), RenderingDevice.INDEX_BUFFER_FORMAT_UINT32, byte_array)
+	primitive.index_array = _TL_Renderer_Factory.rd.index_array_create(primitive.index_buffer, 0, indices.size())
+
+	_generate_vertex_array_from_data(primitive, positions.size())
+
+	return primitive
+
+static func create_inverted_cone(slices : int = 32) -> TL_Procedural_Primitive:
+	var primitive : TL_Procedural_Primitive = TL_Procedural_Primitive.new()
+
+	var positions : PackedVector3Array = []
+	var indices : PackedInt32Array = []
+
+	positions.append(Vector3(0, 0, 0))
+	var apex_index = 0
+
+	for j in range(slices + 1):
+		var u = float(j) / slices
+		var angle = u * TAU
+		var x = cos(angle)
+		var y = sin(angle)
+		var z = -1.0
+
+		positions.append(Vector3(x, y, z))
+
+	for j in range(slices):
+		var i0 = apex_index
+		var i1 = j + 1
+		var i2 = j + 2
+
+		indices.append(i0)
+		indices.append(i2)
+		indices.append(i1)
+
+	var base_center_index = positions.size()
+	positions.append(Vector3(0, 0, -1))
+
+	for j in range(slices):
+		var i0 = base_center_index
+		var i1 = j + 1
+		var i2 = j + 2
+
+		indices.append(i0)
+		indices.append(i1)
+		indices.append(i2)
+
+	var byte_array : PackedByteArray = positions.to_byte_array()
+	primitive.position_buffer = _TL_Renderer_Factory.rd.vertex_buffer_create(byte_array.size(), byte_array)
+
+	byte_array = indices.to_byte_array()
+	primitive.index_buffer = _TL_Renderer_Factory.rd.index_buffer_create(indices.size(), RenderingDevice.INDEX_BUFFER_FORMAT_UINT32, byte_array)
+	primitive.index_array = _TL_Renderer_Factory.rd.index_array_create(primitive.index_buffer, 0, indices.size())
+
+	_generate_vertex_array_from_data(primitive, positions.size())
+
+	return primitive
+
 static func _generate_vertex_array_from_data(primitive : TL_Procedural_Primitive, vertex_count : int, is_2d : bool = false) -> void:
 	var vf_def : TL_VertexFormatDef = TL_VertexFormatDef.new()
 
