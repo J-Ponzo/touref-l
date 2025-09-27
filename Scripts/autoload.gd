590929a946218ba4189d8b36373191b5acfcdd37
jponzo <jeremy.ponzo@gmail.com>
2025-09-27 11:22:04 +0200

    Support native UI an Inputs

--- Scripts/autoload.gd
+++ Scripts/autoload.gd
@@ -11,48 +11,29 @@ var registry : TL_RenderersRegistry_Def = preload("res://renderers_registry.tres
 var renderers : Array[_TL_Renderer]
 var active_renderer_idx : int = NATIVE_RENDERER_IDX
 
-var sub_viewport_container : SubViewportContainer
-var sub_viewport : SubViewport
+var custom_canvas_layer : CanvasLayer
 var texture_rect : TextureRect
-
-var native_sub_viewport_container : SubViewportContainer
-var native_sub_viewport : SubViewport
+var root_vp_rid : RID
 
 func _enter_tree() -> void:
 	for renderer_def : TL_RendererDef in registry.rederer_defs:
 		var renderer_inst = _TL_Renderer_Factory.create_renderer(renderer_def)
 		renderers.append(renderer_inst)
 	
-	native_sub_viewport_container = SubViewportContainer.new()
-	native_sub_viewport_container.stretch = true;
-	native_sub_viewport_container.set_anchors_preset(Control.PRESET_FULL_RECT)
-	
-	native_sub_viewport = SubViewport.new()
-	native_sub_viewport_container.add_child(native_sub_viewport)
-	
-	sub_viewport_container = SubViewportContainer.new()
-	sub_viewport_container.stretch = true;
-	sub_viewport_container.set_anchors_preset(Control.PRESET_FULL_RECT)
-	
-	#todo check drawcalls with default clear mode / update mode
-	sub_viewport = SubViewport.new()
-	sub_viewport.render_target_clear_mode = SubViewport.CLEAR_MODE_NEVER
-	sub_viewport.render_target_update_mode = SubViewport.UPDATE_DISABLED
-	sub_viewport.disable_3d = true;
-	sub_viewport_container.add_child(sub_viewport)
-	
+	root_vp_rid = get_viewport().get_viewport_rid()
+
+	custom_canvas_layer = CanvasLayer.new()
+	custom_canvas_layer.layer = RenderingServer.CANVAS_LAYER_MIN
+	add_child(custom_canvas_layer)
+
 	texture_rect = TextureRect.new()
 	texture_rect.set_anchors_preset(Control.PRESET_FULL_RECT)
-	sub_viewport.add_child(texture_rect)
+	custom_canvas_layer.add_child(texture_rect)
 
 var scene 
 
 func _ready() -> void:
 	scene = get_tree().current_scene
-	scene.get_parent().add_child.call_deferred(native_sub_viewport_container)
-	scene.reparent.call_deferred(native_sub_viewport)
-	
-	add_child(sub_viewport_container)
 	switch_active_renderer(0)
 
 func switch_active_renderer(new_renderer_idx : int) -> bool:
@@ -72,14 +53,15 @@ func _put_renderer_offline(renderer_idx : int) -> void:
 		_put_custom_renderer_offline(renderer_idx)
 
 func _put_native_renderer_offline() -> void:
-	sub_viewport_container.visible = true
-	native_sub_viewport_container.visible = false
+	RenderingServer.viewport_set_disable_3d(root_vp_rid, true)
 	
 func _put_custom_renderer_offline(renderer_idx : int) -> void:
 	texture_rect.texture = null
 	active_renderer_idx = INVALID_RENDERER_IDX
 	renderers[renderer_idx]._cleanup()
 	renderers[renderer_idx].scene_proxy._cleanup()
+
+	custom_canvas_layer.visible = false
 
 func _put_renderer_online(renderer_idx : int) -> void:
 	if renderer_idx == NATIVE_RENDERER_IDX:
@@ -89,8 +71,7 @@ func _put_renderer_online(renderer_idx : int) -> void:
 	active_renderer_idx = renderer_idx
 
 func _put_native_renderer_online() -> void:
-	sub_viewport_container.visible = false
-	native_sub_viewport_container.visible = true
+	RenderingServer.viewport_set_disable_3d(root_vp_rid, false)
 
 func _put_custom_renderer_online(renderer_idx : int) -> void:
 	renderers[renderer_idx]._setup()
@@ -99,6 +80,8 @@ func _put_custom_renderer_online(renderer_idx : int) -> void:
 	var render_target_from_rd : Texture2DRD = Texture2DRD.new()
 	render_target_from_rd.texture_rd_rid = renderers[renderer_idx].get_render_target()
 	texture_rect.texture = render_target_from_rd
+
+	custom_canvas_layer.visible = true
 	
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
